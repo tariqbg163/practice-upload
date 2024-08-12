568d04229f27daf63bd6f9c9607accb6ace92a75
Tariq Ullah <tariqbg163@gmail.com>
2024-08-12 19:00:38 +0500

    Adding all data once

---
+++ list.py
@@ -0,0 +1,7 @@
+list=[44,55,66,77]
+i=0
+# print(len(list))
+# print(list[0])
+while i < len(list):
+    print(f'{list[i]}' , end= " ")
+    i=i+1
---
+++ while.py
@@ -0,0 +1,4 @@
+i=4
+while i<=7:
+    print('this is while loop')
+    i=i+1
